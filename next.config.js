4cddf818fe611609f0e4fb64b705584762a15472
Amarildo <amarildokola36@gmail.com>
2023-04-09 14:10:26 +0200

    Navbar and Image optimization added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["www.si.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,10 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
-import { Container } from 'react-bootstrap';
+import { Container, SSRProvider } from 'react-bootstrap';
 import styles from "@/styles/App.module.css";
+import NavBar from '@/components/NavBar';
+import NextNProgress from "nextjs-progressbar";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,9 +19,13 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container className={styles.pageContainer}>
-        <Component {...pageProps} />
-      </Container>
+      <SSRProvider>
+        <NextNProgress />
+        <NavBar />
+        <Container className={styles.pageContainer}>
+          <Component {...pageProps} />
+        </Container>
+      </SSRProvider>
     </div>
   );
 }
